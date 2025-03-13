d51e2bc33c12d1249701672cf47ff354f2cfa39e
emarkula24 <emarkula24@student.oulu.fi>
2025-03-13 13:51:11 +0200

    first commit

---
+++ helpers/db.js
@@ -0,0 +1,30 @@
+const { Pool } = require("pg")
+
+require("dotenv").config()
+
+const query = (sql, values = []) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            const pool = openDb()
+            const result = await pool.query(sql, values)
+            resolve(result)
+        }catch(error) {
+            reject(error.message)
+        }
+    })
+}
+
+const openDb = () => {
+    const pool = new Pool({
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        port: process.env.POSTGRES_PORT
+    })
+    return pool;
+}
+
+module.exports = {
+    query
+}
